ff35d6d2907384e1d08617d58d079042a84b2219
Rakesh Cheekatimala <rakesh.cheekatimala@singtel.com>
2020-05-28 12:13:47 +0800

    RestuarantService implementation

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -0,0 +1,19 @@
+package com.upgrad.FoodOrderingApp.service.businness;
+
+import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RestaurantService {
+    @Autowired
+    RestaurantDao restaurantDao;
+
+    public List<RestaurantEntity> getAllResturants(){
+        return restaurantDao.getAllResturants();
+    }
+
+}
