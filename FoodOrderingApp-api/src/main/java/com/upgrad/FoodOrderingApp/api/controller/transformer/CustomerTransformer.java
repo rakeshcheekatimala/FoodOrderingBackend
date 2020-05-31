5c59667ae14f3474197bb112f75409eaae05bd10
Rakesh Cheekatimala <rakesh.cheekatimala@singtel.com>
2020-05-31 13:57:45 +0800

    toLoginResponse response

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/transformer/CustomerTransformer.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/transformer/CustomerTransformer.java
@@ -1,4 +1,14 @@
 package com.upgrad.FoodOrderingApp.api.controller.transformer;
 
+import com.upgrad.FoodOrderingApp.api.model.LoginResponse;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+
 public class CustomerTransformer {
+
+    public static  LoginResponse toLoginResponse(CustomerEntity customerEntity){
+        LoginResponse loginResponse = new LoginResponse().id(customerEntity.getUuid()).firstName(customerEntity.getFirstName())
+                .lastName(customerEntity.getLastName()).emailAddress(customerEntity.getEmail()).contactNumber(customerEntity.getContactNumber())
+                .message("LOGGED IN SUCCESSFULLY");
+        return loginResponse;
+    }
 }
