989454cc33a0822863712b6ced52e93a39cd4439
Rakesh Cheekatimala <rakesh.cheekatimala@singtel.com>
2020-05-31 19:44:37 +0800

    /customer/logout endpoint implementation

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
@@ -2,11 +2,18 @@ package com.upgrad.FoodOrderingApp.api.controller;
 
 
 import com.upgrad.FoodOrderingApp.api.controller.provider.BasicAuthDecoder;
+import com.upgrad.FoodOrderingApp.api.controller.provider.BearerAuthDecoder;
 import com.upgrad.FoodOrderingApp.api.controller.transformer.CustomerTransformer;
 import com.upgrad.FoodOrderingApp.api.model.LoginResponse;
+import com.upgrad.FoodOrderingApp.api.model.LogoutResponse;
+import com.upgrad.FoodOrderingApp.service.businness.CustomerAuthService;
+import com.upgrad.FoodOrderingApp.service.businness.CustomerAuthTokenVerifier;
 import com.upgrad.FoodOrderingApp.service.businness.CustomerService;
+import com.upgrad.FoodOrderingApp.service.dao.CustomerAuthDao;
 import com.upgrad.FoodOrderingApp.service.entity.CustomerAuthEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
 import com.upgrad.FoodOrderingApp.service.exception.AuthenticationFailedException;
+import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -25,6 +32,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CustomerController {
         @Autowired
         CustomerService customerService;
+
+        @Autowired
+        CustomerAuthService customerAuthService;
 
         @RequestMapping(method = RequestMethod.POST, path = "/customer/login", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
         public ResponseEntity<LoginResponse> login(@RequestHeader("authorization")  String authorization) throws AuthenticationFailedException {
@@ -48,5 +58,36 @@ public class CustomerController {
             else {
                 throw new AuthenticationFailedException("ATH-001", "This contact number has not been registered!");
             }
+        }
+
+    @RequestMapping(method= RequestMethod.POST, path="/customer/logout", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<LogoutResponse> logout(@RequestHeader("authorization") final String authorization)
+            throws AuthorizationFailedException, AuthenticationFailedException {
+
+        // authDecoder splits the authorization to extract the token
+        final BearerAuthDecoder authDecoder = new BearerAuthDecoder(authorization);
+        String accessToken = authDecoder.getAccessToken();
+        CustomerAuthEntity customerAuthEntity = customerAuthService.getCustomerByToken(accessToken);
+
+        if(customerAuthEntity == null){
+            throw new AuthorizationFailedException("ATHR-001", "Customer is not Logged in.");
+        }
+        CustomerAuthTokenVerifier customerAuthTokenVerifier = new CustomerAuthTokenVerifier(customerAuthEntity);
+
+        if(customerAuthTokenVerifier.hasLoggedOut()){
+            throw new AuthorizationFailedException("ATHR-002", "Customer is logged out. Log in again to access this endpoint.");
+        }
+
+        if (customerAuthTokenVerifier.hasExpired()) {
+            throw new AuthorizationFailedException("ATHR-003", "Your session is expired. Log in again to access this endpoint.");
+        }
+
+        // Calls the logout method by passing the bearer token
+        final CustomerAuthEntity logoutEntity = customerService.logout(customerAuthEntity);
+
+        LogoutResponse logoutResponse = CustomerTransformer.toLogoutResponse(logoutEntity.getCustomer());
+        // Returns the LogoutResponse with OK http status
+        return new ResponseEntity<LogoutResponse>(logoutResponse, HttpStatus.OK);
+
     }
 }
